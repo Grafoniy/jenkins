c64ad32f5983301c55335eb40c54c5916853fda6
Grafoniy <105426370+Grafoniy@users.noreply.github.com>
2022-08-22 10:38:00 +0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,2 @@
 select * from family order by auto_wiki limit 3 ;
+https://lms.skillfactory.ru/courses/course-v1:SkillFactory+DEVOPS-3.0+2021/courseware/745596c246b742a79ea84c3e4ccf27b2/473707eb849144d4bb516590ad0cc390/?child=first
